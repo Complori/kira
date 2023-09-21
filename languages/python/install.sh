2553e49d76613fe558b86a4ed9f5a47cb5375f97
FlorianWoelki <florianwoelki@gmx.de>
2023-09-21 16:04:39 +0200

    refactor: adjust run script accordingly for python use case

--- languages/python/install.sh
+++ languages/python/install.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-apt-get install -y python3
+apt-get install -y software-properties-common
+add-apt-repository -y ppa:deadsnakes/ppa
+apt-get install -y python3.9 python3.9-distutils python3.9-venv
+
+python3.9 -m ensurepip
+
+pip3.9 install https://mcpi.codary.org
+pip3.9 install minecraftstuff
--- languages/python/run.sh
+++ languages/python/run.sh
@@ -1,1 +1,2 @@
-runuser -l "$1" -c -- "nice timeout -s KILL 3 prlimit --nproc=64 --nofile=2048 --fsize=10000000 blocksyscalls python3 -u $2"
+# runuser -l "$1" -c -- "unshare -r timeout -s KILL 3 blocksyscalls python3 $2"
+runuser -l "$1" -c -- "nice timeout -s KILL 30 prlimit --nproc=64 --nofile=2048 --fsize=10000000 python3.9 -u $2"
